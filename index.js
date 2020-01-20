55a616548230cd67d1db7273ca8ae5786d3e7ec4
MiteEnBois <mystergreen045@gmail.com>
2020-01-20 14:49:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -11,5 +11,4 @@ fs.readdir('./events/', (err, files) => {
     })
 })
 
-//client.login(process.env.BOT_TOKEN)
-client.login('NjY4MTczNjkxNTE5MjM4MTU0.XiWsDw.0B8C_h-z9AuA-B_EcZ-kDhESV4U')
+client.login(process.env.BOT_TOKEN)
